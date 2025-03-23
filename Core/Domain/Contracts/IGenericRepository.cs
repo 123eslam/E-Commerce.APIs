1fe2b6e2e74e9156fee99d49d6e7385dbacfa86a
123eslam <eslameltamalawy124@gmail.com>
2025-03-23 23:23:45 +0200

    9 - GenericRepository

---
+++ Core/Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Domain.Contracts
+{
+    public interface IGenericRepository<TEntity,Tkey> where TEntity : BaseEntity<Tkey>
+    {
+        //GetById ,GetAll ,Update ,Add ,Delete
+        Task<TEntity?> GetByIdAsync(Tkey id);
+        Task<IEnumerable<TEntity>> GetAllAsync(bool asNoTracking = true);
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
--- Infrastructure/Persistance/Global/GlobalUsing.cs
+++ Infrastructure/Persistance/Global/GlobalUsing.cs
@@ -3,4 +3,6 @@ global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using System.Reflection;
 global using Domain.Contracts;
+global using Domain.Entities;
+global using Persistance.Data;
 
