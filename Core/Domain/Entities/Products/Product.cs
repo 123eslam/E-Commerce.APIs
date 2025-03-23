85347be07aec7e76322766a82eac34d3e6187aa3
123eslam <eslameltamalawy124@gmail.com>
2025-03-23 17:13:05 +0200

    6 - Product - ProductType - ProductBrand Entities

---
+++ Core/Domain/Entities/Products/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Entities.Products
+{
+    public class Product : BaseEntity<int>
+    {
+        //Id
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string PictureUrl { get; set; } = default!;
+        public decimal Price { get; set; } = default!;
+        //Navigation property one [ProductBrand]
+        public ProductBrand ProductBrand { get; set; } = default!;
+        //FK
+        public int BrandId { get; set; }
+        //Navigation property one [ProductType]
+        public ProductType ProductType { get; set; } = default!;
+        //FK
+        public int ProductId { get; set; }
+    }
+}
