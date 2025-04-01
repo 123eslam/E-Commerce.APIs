b089899e19be4712ab3404eaa9bd4f275b625b5b
123eslam <eslameltamalawy124@gmail.com>
2025-04-01 10:39:10 +0200

    Part 05 Specifications

---
+++ Core/Domain/Contracts/Specifications.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain.Contracts
+{
+    public abstract class Specifications<T> where T : class
+    {
+        protected Specifications(Expression<Func<T, bool>>? criteria)
+        {
+            Criteria = criteria;
+        }
+        public Expression<Func<T, bool>>? Criteria { get; }
+        public List<Expression<Func<T, object>>> IncludeExpressions { get; } = new();
+        protected void AddInclude(Expression<Func<T, object>> includeExpression) 
+            => IncludeExpressions.Add(includeExpression);
+    }
+}
+// _dbContext.Set<T>().Where(Expression).Include().Orderby()
+// Func<T, bool>
+// Where Expression<Func<T, bool>>
+// Include List<Expression<Func<T, object>>>
---
+++ Core/Services/Specifications/ProductWithBrandAndTypeSpecifications.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Contracts;
+using Domain.Entities.Products;
+
+namespace Services.Specifications
+{
+    internal class ProductWithBrandAndTypeSpecifications : Specifications<Product>
+    {
+        //Use to retrieve a product by id
+        public ProductWithBrandAndTypeSpecifications(int id)
+            : base(product => product.Id == id)
+        {
+            AddInclude(product => product.ProductBrand);
+            AddInclude(product => product.ProductType);
+        }
+
+        //Use to retrieve all products
+        public ProductWithBrandAndTypeSpecifications()
+            : base(null)
+        {
+            AddInclude(product => product.ProductBrand);
+            AddInclude(product => product.ProductType);
+        }
+    }
+}
