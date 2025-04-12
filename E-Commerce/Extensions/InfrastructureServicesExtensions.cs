92100286fb3937605e957c00d99239a7930447d9
123eslam <eslameltamalawy124@gmail.com>
2025-04-12 23:56:56 +0200

    4 - Basket Repository

--- E-Commerce/Extensions/InfrastructureServicesExtensions.cs
+++ E-Commerce/Extensions/InfrastructureServicesExtensions.cs
@@ -3,6 +3,7 @@ using Persistance.Data.DataSeeding;
 using Persistance.Data;
 using Persistance.Repositories;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 namespace E_Commerce.Extensions
 {
@@ -17,6 +18,8 @@ namespace E_Commerce.Extensions
             {
                 optios.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
             });
+            //Redis
+            services.AddSingleton<IConnectionMultiplexer>(services => ConnectionMultiplexer.Connect(configuration.GetConnectionString("RedisConnection")!));
             return services;
         }
     }
