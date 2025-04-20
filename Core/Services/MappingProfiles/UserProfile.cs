1f9cc1bbe982ae72265b9c60a6cc1918f31a0ef1
123eslam <eslameltamalawy124@gmail.com>
2025-04-20 03:52:43 +0200

    8 - Authentication Service Get & Update Address , Get user , Check Email

---
+++ Core/Services/MappingProfiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Domain.Entities.Identity;
+using Shared.OrderDtos;
+
+namespace Services.MappingProfiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<Adress, AddressDto>().ReverseMap();
+        }
+    }
+}
