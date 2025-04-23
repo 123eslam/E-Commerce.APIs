32d0737f7caa052374fb4f10ec50a1522b4e101f
123eslam <eslameltamalawy124@gmail.com>
2025-04-23 13:31:00 +0200

    implement authorization for swagger

--- E-Commerce/Extensions/PresentitionServicesExtensions.cs
+++ E-Commerce/Extensions/PresentitionServicesExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using E_Commerce.Factories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
 
 namespace E_Commerce.Extensions
@@ -16,7 +17,32 @@ namespace E_Commerce.Extensions
                         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
                     });
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(option =>
+            {
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
             services.Configure<ApiBehaviorOptions>(options =>
             {
                 options.InvalidModelStateResponseFactory = ApiResponseFactory.CustomValidationErrorResponse;//Func => Return IActionResult ,Take ActionContext
