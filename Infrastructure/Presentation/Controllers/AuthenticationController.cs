cc446c82c0ff4f90df5214bae6c79cdc3c1707dc
123eslam <eslameltamalawy124@gmail.com>
2025-04-26 19:23:51 +0300

    9 - Authentication Controller Get & Update Address , Get user , Check Email

--- Infrastructure/Presentation/Controllers/AuthenticationController.cs
+++ Infrastructure/Presentation/Controllers/AuthenticationController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Services.Abstraction;
 using Shared.IdentityDtos;
+using Shared.OrderDtos;
 using System.Net;
+using System.Security.Claims;
 
 namespace Presentation.Controllers
 {
@@ -20,6 +23,40 @@ namespace Presentation.Controllers
         {
             var user = await ServiceManager.AuthenticationService.Login(loginDto);
             return Ok(user);
+        }
+        [HttpGet("EmailExists")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> CheckEmailExists(string email)
+        {
+            var result = await ServiceManager.AuthenticationService.CheckEmailExists(email);
+            return Ok(result);
+        }
+        [Authorize]
+        [HttpGet]
+        [ProducesResponseType(typeof(UserResultDto), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<UserResultDto>> GetUserByEmail()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var user = await ServiceManager.AuthenticationService.GetUserByEmail(email);
+            return Ok(user);
+        }
+        [Authorize]
+        [HttpGet("Address")]
+        [ProducesResponseType(typeof(AddressDto), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<AddressDto>> GetAddress()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var address = await ServiceManager.AuthenticationService.GetUserAddress(email);
+            return Ok(address);
+        }
+        [Authorize]
+        [HttpPut("Address")]
+        [ProducesResponseType(typeof(AddressDto), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<AddressDto>> UpdateAddress(AddressDto addressDto)
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var address = await ServiceManager.AuthenticationService.UpdateUserAddress(addressDto, email);
+            return Ok(address);
         }
     }
 }
